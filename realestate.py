101f02edfe8c0b055e12634cb16e25956547e538
Kevin Tang <ktang124@umd.edu>
2023-04-08 17:52:59 -0400

    uploaded the updated company location spreadsheet and adapted the real estate.py to work with itt

--- realestate.py
+++ realestate.py
@@ -1,55 +1,110 @@
 import requests
 import json
+import csv
 
 url = "https://us-real-estate.p.rapidapi.com/v2/for-rent-by-zipcode"
 
 headers = {
-	"X-RapidAPI-Key": "09bca80104msh15e694ff58ae934p1dbb1djsnc6a79bbc62eb",
-	"X-RapidAPI-Host": "us-real-estate.p.rapidapi.com"
+    "X-RapidAPI-Key": "09bca80104msh15e694ff58ae934p1dbb1djsnc6a79bbc62eb",
+    "X-RapidAPI-Host": "us-real-estate.p.rapidapi.com"
 }
 
+
+def companyAddress(company: str, location: str):
+    # csv, Adobe,San Jose,CA,37.331761,-121.893546,"345 Park Ave, San Jose, CA 95110"
+    #line: ['Uber', 'New York City', 'NY', '40.7111415', '-74.0092626', '1400 Broadway Suite 1801, New York, NY 10018']
+    filename = "companyOffices.csv"
+    data = open(filename, 'r')
+    for line in csv.reader(data):
+        comp, city, state, longitude, latitude, address = line
+        #zip = int(address[len(address)-5:len(address)])
+        if comp == company and city == location:
+            return address
+
+    return ''
+
+
+print(companyAddress('Adobe', 'San Jose'))
+
+
+def companyCities(company: str):
+    filename = "companyOffices.csv"
+    data = open(filename, 'r')
+    res = []
+    for line in csv.reader(data):
+        comp, city, state, longitude, latitude, address = line
+        if comp == company:
+            res.append(city)
+    return res
+
+
+def queryCompanyHousing(company: str, location: str, limit, price_min, price_max):
+    filename = "companyOffices.csv"
+    data = open(filename, 'r')
+    for line in csv.reader(data):
+        comp, city, state, longitude, latitude, address = line
+        zip = int(address[len(address)-5:len(address)])
+        if comp == company and city == location:
+            return query(zip, limit, price_min, price_max)
+
+    return ''
+
+
 def query(zip, limit, price_min, price_max):
-    querystring = {"zipcode":zip,"limit":limit,"offset":"0",
-                   "sort":"lowest_price","price_min":price_min,
-                   "price_max":price_max,"property_type":"apartment"}
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    querystring = {"zipcode": zip, "limit": limit, "offset": "0",
+                   "sort": "lowest_price", "price_min": price_min,
+                   "price_max": price_max, "property_type": "apartment"}
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
     json_data = json.loads(response.text)
     apts = ((json_data['data'])['home_search'])['results']
     return apts
 
+
 def get_address(apt):
     address = (apt['location'])['address']
-    str = address['line'] + ', ' + address['city'] + ', '+ address['state_code'] + ', ' + address['postal_code']
+    str = address['line'] + ', ' + address['city'] + ', ' + \
+        address['state_code'] + ', ' + address['postal_code']
     return str
+
 
 def get_coordinate(apt):
     coordinate = ((apt['location'])['address'])['coordinate']
     return coordinate
 
+
 def get_href(apt):
     href = apt['href']
     return href
+
 
 def get_image_href(apt):
     image_href = (apt['primary_photo'])['href']
     return image_href
 
+
 def get_beds(apt):
-    categories = ['beds','beds_min','beds_max']
-    beds = max((apt['description'][beds]) if apt['description'][beds] else 0 for beds in categories)
+    categories = ['beds', 'beds_min', 'beds_max']
+    beds = max((apt['description'][beds]) if apt['description']
+               [beds] else 0 for beds in categories)
     return beds
 
+
 def get_baths(apt):
-    categories =['baths','baths_full','baths_max','baths_min','baths_full_calc','baths_partial_calc','baths_1qtr']
-    baths = max(apt['description'][baths] if apt['description'][baths] else 0 for baths in categories)
+    categories = ['baths', 'baths_full', 'baths_max', 'baths_min',
+                  'baths_full_calc', 'baths_partial_calc', 'baths_1qtr']
+    baths = max(apt['description'][baths] if apt['description']
+                [baths] else 0 for baths in categories)
     return baths
 
+
 def get_price(apt):
-    categories =['list_price_max','list_price','list_price_min']
+    categories = ['list_price_max', 'list_price', 'list_price_min']
     price = max(apt[prices] if apt[prices] else 0 for prices in categories)
     return price
 
-res = query(80301, 10, 0, 1000)
+
+res = queryCompanyHousing('Adobe', 'San Jose', 10, 0, 1000)
 
 # f = open('sample.json')
 # json_data = json.load(f)
